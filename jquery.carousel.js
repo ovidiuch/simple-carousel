ad8eef452a922717c8289803d5f302e89adfc7a5
Ovidiu Chereches <skidding@gmail.com>
2012-08-24 15:59:37 +0300

    Added nav bar

--- jquery.carousel.js
+++ jquery.carousel.js
@@ -3,8 +3,7 @@
     this.wrapper = wrapper;
     this.options = $.extend({}, Carousel.defaults, options);
     // Wait until all images are loaded
-    var carousel = this;
-    var left = $(this.wrapper).find('img').load(function() {
+    var carousel = this, left = $(this.wrapper).find('img').load(function() {
       if (!--left) {
         carousel.init();
       }
@@ -16,11 +15,23 @@
     this.list = $(this.wrapper).find('ul');
     // Set list fixed width to it current automatic width
     this.list.width(this.list.width());
+    // Mark carousel ready
+    $(this.wrapper).addClass('ready');
     // Define the children property as a method in order for it
     // to update its child order every time it changes
     this.children = function() {
       return carousel.list.find('> li');
     };
+    // Disable carousel if it has less than two items
+    if (this.children().length < 2) {
+      // Hide navigation
+      $(this.wrapper).find('.nav').hide();
+      return;
+    }
+    // Save order index as their position is going to change
+    this.children().each(function(i) {
+      $(this).data('index', i);
+    });
     // Set click events on arrow buttons
     this.arrow = $(this.wrapper).find('.arrow').click(function(e) {
       e.preventDefault();
@@ -28,8 +39,6 @@
     });
     // Set first element as current
     this.current = this.children().first();
-    // Mask carousel ready
-    $(this.wrapper).addClass('ready');
     // Slide and update carousel for the first time
     this.slide();
     this.update();
@@ -72,7 +81,10 @@
     });
   };
   Carousel.prototype.update = function() {
-    // show display
+    // Populate navigation variables
+    $(this.wrapper).find('.total').html(this.children().length);
+    $(this.wrapper).find('.current').html(this.current.data('index') + 1);
+    // Run user callback, if present
     if (typeof(this.options.callback) == 'function') {
       this.options.callback.call(this, this.current.find('img'));
     }
